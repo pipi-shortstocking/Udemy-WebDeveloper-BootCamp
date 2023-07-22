e33499af2074c73465e286e58d3ace80ed2f3b92
pipi-shortstocking <jannie435@naver.com>
2023-07-22 17:12:05 +0900

    conditional

--- js 강의/12_Boolean_Logic/Conditionals/app.js
+++ js 강의/12_Boolean_Logic/Conditionals/app.js
@@ -1,64 +1,48 @@
-// =====================
-// BASIC IF/ELSE
-// =====================
-let random = Math.random();
-if (random < 0.5) {
-    console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
-} else {
-    console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!")
-}
-console.log(random);
-
-// =====================
-// PROMPT EXAMPLE
-// =====================
-
-const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
-
-if (dayOfWeek === 'monday') {
-    console.log("UGHHH I HATE MONDAYS!")
-} else if (dayOfWeek === 'saturday') {
-    console.log("YAY I LOVE SATURDAYS!")
-} else if (dayOfWeek === 'friday') {
-    console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
-} else {
-    console.log("MEH")
-}
+// let random = Math.random();
+// if (random < 0.5){
+//     console.log("YOUR NUNMBER IS LESS THAN 0.5!!")
+// } else {
+//     console.log("YOUR NUNMBER IS GREATER (OR EQUAL) THAN 0.5!!")
+// }
+// console.log(random);
 
 
 
-// =====================
-// TICKET PRICE EXAMPLE
-// =====================
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase()
+
+// if(dayOfWeek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS!")
+// } else if (dayOfWeek === 'saturday'){
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfWeek === 'friday'){
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+// } else {
+//     console.log("MEH")
+// }
 
 // 0-5 - FREE
-// 5 - 10 CHILD $10 
-// 10 - 65 ADULT $20 
+// 5 - 10 CHILD $10
+// 10 - 65 ADULT $20
 // 65+ SENIOR $10
 
-const age = 890;
+// const age = 89;
 
-if (age < 5) {
-    console.log("You are a baby.  You get in for free!")
-} else if (age < 10) {
-    console.log("You are a child.  You pay $10")
-} else if (age < 65) {
-    console.log("You are an adult. You pay $20")
-} else {
-    console.log("You are a senior. You pay $10")
-}
+// if (age < 5) {
+//     console.log("Your are a baby.  You get in for free!")
+// } else if (age < 10) {
+//     console.log("You are a child.  You pay $10")
+// } else if (age < 65) {
+//     console.log("Your are an adult.  You pay $20")
+// } else {
+//     console.log("Your are a senior. You pay $10")
+// }
 
-
-// =====================
-// NESTING CONDITIONALS
-// =====================
-
-const password = prompt("please enter a new password");
+const password = prompt("please a new password!");
 
 // Password must be 6+ characters
 if (password.length >= 6) {
-    // Password cannot include space
-    if (password.indexOf(' ') === -1) {
+    if(password.indexOf(' ') === -1){
         console.log("Valid Password!");
     } else {
         console.log("Password cannot contain spaces!")
@@ -66,6 +50,4 @@ if (password.length >= 6) {
 } else {
     console.log("PASSWORD TOO SHORT! Must be 6+ characters")
 }
-
-
-
+// Password cannot include space
