c5651941a0784584e8d314693a803682b6dbef1e
pipi-shortstocking <jannie435@naver.com>
2023-07-27 14:47:34 +0900

    for loop

--- js 강의/Loops_CODE/ForLoops/app.js
+++ js 강의/Loops_CODE/ForLoops/app.js
@@ -1,6 +1,3 @@
-// ======================================
-// WITHOUT ANY LOOPS...LOTS OF REPETITION
-// ======================================
 // console.log(1)
 // console.log(2)
 // console.log(3)
@@ -12,41 +9,31 @@
 // console.log(9)
 // console.log(10)
 
-// ======================================
-// OUR FIRST FOR LOOP
-// ======================================
+// for(let num = 1; num<=10;num++){
+//     console.log("I'M IN THE LOOP BODY!!!")
+//     console.log(num)
+// }
 
-for (let num = 1; num <= 10; num += 1) {
-    console.log(num);
-}
-
-// ======================================
-// MORE EXAMPLES OF FOR LOOPS!
-// ======================================
-
-// for (let i = 1; i <= 20; i += 2) {
+// for(let i = 1; i<=20;i+=2){
 //     console.log(i)
 // }
 
-// for (let i = 100; i >= 0; i -= 10) {
+// for(let i = 100;i>=0;i-=10){
 //     console.log(i);
 // }
 
-// for (let i = 10; i <= 1000; i *= 10) {
+// for(let i = 10;i<=1000;i*=10){
 //     console.log(i);
 // }
 
 // DO NOT RUN THIS LOOP
 // INFINITE LOOP!! BAD!!
-// ================================
-// ================================
-// for (let i = 20; i >= 0; i++) {
+// ===========================
+// ===========================
+// for(let i = 20; i>=0; i++) {
 //     console.log(i)
 // }
-// ================================
-// ================================
-
-
-
-
-
+// ===========================
+// ===========================
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
