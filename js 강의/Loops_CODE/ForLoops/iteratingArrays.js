1b518b0420a39494fb2da87453af064b2c122a9a
pipi-shortstocking <jannie435@naver.com>
2023-08-03 14:08:13 +0900

    arrayLoop

--- js 강의/Loops_CODE/ForLoops/iteratingArrays.js
+++ js 강의/Loops_CODE/ForLoops/iteratingArrays.js
@@ -318,10 +318,10 @@ const animals = [
     "Zorilla"
 ]
 
-for (let i = 0; i < animals.length; i++) {
-    console.log(i, animals[i])
-}
-
-// for (let i = animals.length - 1; i >= 0; i--) {
-//     console.log(animals[i])
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(i, animals[i])
 // }
+
+for(let i = animals.length - 1; i >= 0; i--) {
+    console.log(animals[i])
+}
