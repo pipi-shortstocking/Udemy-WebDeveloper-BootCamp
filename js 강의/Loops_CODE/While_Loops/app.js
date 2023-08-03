9154503a4e644ddbc83778d5fd7fef3abc5f49bc
pipi-shortstocking <jannie435@naver.com>
2023-08-03 15:11:17 +0900

    guess game

--- js 강의/Loops_CODE/While_Loops/app.js
+++ js 강의/Loops_CODE/While_Loops/app.js
@@ -19,7 +19,37 @@
 // }
 // console.log("OK YOU WIN!")
 
-for(let i = 0; i < 1000; i++) {
-    console.log(i);
-    if(i === 100 )break;
+// for(let i = 0; i < 1000; i++) {
+//     console.log(i);
+//     if(i === 100 )break;
+// }
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt("enter your first guess! (Type 'q' to quit)");
+let attempts = 1;
+
+while(parseInt(guess) !== targetNum) {
+    if(guess === 'q') break;
+    guess = parseInt(guess)
+    if(guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:")
+        attempts++;
+    } else if (guess < targetNum) {
+        guess = prompt("Too low! Enter a new guess:")
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit");
+    }
+}
+if(guess == 'q') {
+    console.log("OK, YOU QUIT!")
+} else {
+    console.log("CONGRATS YOU WIN!")
+    console.log(`You got it! It took you ${attempts} guesses`)
 }
