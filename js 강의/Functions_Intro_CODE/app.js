11f2724b6964880a5601cf24d0ecb8ff7d205648
pipi-shortstocking <jannie435@naver.com>
2023-08-07 14:10:42 +0900

    arguments

--- js 강의/Functions_Intro_CODE/app.js
+++ js 강의/Functions_Intro_CODE/app.js
@@ -3,15 +3,35 @@ function singSong(){
     console.log("RE");
     console.log("MI");
 }
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for(let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+function add(x,y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
