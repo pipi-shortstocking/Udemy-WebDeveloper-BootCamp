8722efd84fa13d7bf3289a99ae49f2857b483574
pipi-shortstocking <jannie435@naver.com>
2023-08-08 13:58:38 +0900

    this

--- js 강의/More_Functions_CODE/Methods/app.js
+++ js 강의/More_Functions_CODE/Methods/app.js
@@ -7,3 +7,15 @@ const myMath = {
         return num ** 3;
     }
 }
+
+const cat = {
+    name: 'Blue Steele',
+    color: 'grey',
+    breed: 'scottish fold',
+    meow() {
+        console.log("THIS IS:", this)
+        console.log(`${this.name} says MEOWWWW`);
+    }
+}
+
+const meow2 = cat.meow;
