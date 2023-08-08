eefc23f249014620ba514a390886999af2bf7385
pipi-shortstocking <jannie435@naver.com>
2023-08-08 13:06:17 +0900

    scope

--- js 강의/More_Functions_CODE/Scope/app.js
+++ js 강의/More_Functions_CODE/Scope/app.js
@@ -6,9 +6,38 @@
 // collectEggs();
 // console.log(totalEggs);
 
-let bird = 'Scarlet Macaw';
-function birdWatch() {
-    // let bird = 'Great Blue Heron';
-    console.log(bird);
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     // let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch();
+
+// let radius = 8;
+// if(radius > 0){
+//     const PI = 3.14159;
+//     let msg = 'HIII!'
+// }
+
+// console.log(radius);
+// console.log(PI);
+
+// for(let i = 0; i < 5; i++) {
+//     let msg = "ASKLDJAKLSD";
+//     console.log(msg)
+// }
+// console.log(msg)
+// console.log(i)
+
+function bankRobbery(){
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman']
+    function cryForHelp() {
+        function inner() {
+            for(let hero of heroes) {
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+            }
+        }
+        inner();
+    }
+    cryForHelp();
 }
-birdWatch();
