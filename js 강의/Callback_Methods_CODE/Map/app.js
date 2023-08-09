d4941ba8d34347285d932c49dad12744819d4c38
pipi-shortstocking <jannie435@naver.com>
2023-08-09 14:24:07 +0900

    map

--- js 강의/Callback_Methods_CODE/Map/app.js
+++ js 강의/Callback_Methods_CODE/Map/app.js
@@ -1,8 +1,8 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-const doubles = numbers.map(function (num) {
-    return num * 2;
-})
+// const doubles = numbers.map(function(num) {
+//     return num * 2;
+// })
 
 
 const movies = [
@@ -24,8 +24,6 @@ const movies = [
     }
 ]
 
-const titles = movies.map(function (movie) {
+const titles = movies.map(function(movie) {
     return movie.title.toUpperCase();
 })
-
-
