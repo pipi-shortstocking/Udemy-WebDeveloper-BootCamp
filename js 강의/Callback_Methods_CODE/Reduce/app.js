fb9c0abb29d592cabe593dab1578051c6c03654e
pipi-shortstocking <jannie435@naver.com>
2024-01-15 01:47:31 +0900

    reduce 함수

--- js 강의/Callback_Methods_CODE/Reduce/app.js
+++ js 강의/Callback_Methods_CODE/Reduce/app.js
@@ -1,81 +1,80 @@
-const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+const prices = [9.99, 1.5, 19.99, 49.99, 30.5];
 
 // let total = 0;
 // for (let price of prices) {
-//     total += price
+//   total += price;
 // }
-// console.log(total)
+// console.log(total);
 
 // const total = prices.reduce((total, price) => {
-//     return total + price
-// })
+//   return total + price;
+// });
 
-const total = prices.reduce((total, price) => total + price)
+const total = prices.reduce((total, price) => total + price);
 
 const minPrice = prices.reduce((min, price) => {
-    if (price < min) {
-        return price;
-    }
-    return min;
-})
+  if (price < min) {
+    return price;
+  }
+  return min;
+});
 
 const movies = [
-    {
-        title: 'Amadeus',
-        score: 99,
-        year: 1984
-    },
-    {
-        title: 'Sharknado',
-        score: 35,
-        year: 2013
-    },
-    {
-        title: '13 Going On 30',
-        score: 70,
-        year: 2004
-    },
-    {
-        title: 'Stand By Me',
-        score: 85,
-        year: 1986
-    },
-    {
-        title: 'Waterworld',
-        score: 62,
-        year: 1995
-    },
-    {
-        title: 'Jingle All The Way',
-        score: 71,
-        year: 1996
-    },
-    {
-        title: 'Parasite',
-        score: 95,
-        year: 2019
-    },
-    {
-        title: 'Notting Hill',
-        score: 77,
-        year: 1999
-    },
-    {
-        title: 'Alien',
-        score: 90,
-        year: 1979
-    }
-]
+  {
+    title: "Amadeus",
+    score: 99,
+    year: 1984,
+  },
+  {
+    title: "Sharknado",
+    score: 35,
+    year: 2013,
+  },
+  {
+    title: "13 Going On 30",
+    score: 70,
+    year: 2004,
+  },
+  {
+    title: "Stand By Me",
+    score: 85,
+    year: 1986,
+  },
+  {
+    title: "Waterworld",
+    score: 62,
+    year: 1995,
+  },
+  {
+    title: "Jingle All The Way",
+    score: 71,
+    year: 1996,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+    year: 2019,
+  },
+  {
+    title: "Notting Hill",
+    score: 77,
+    year: 1999,
+  },
+  {
+    title: "Alien",
+    score: 90,
+    year: 1979,
+  },
+];
 
 const highestRated = movies.reduce((bestMovie, currMovie) => {
-    if (currMovie.score > bestMovie.score) {
-        return currMovie;
-    }
-    return bestMovie;
-})
+  if (currMovie.score > bestMovie.score) {
+    return currMovie;
+  }
+  return bestMovie;
+});
 
-
-// We can provide an initial value as the 2nd arg to reduce:
+// reduce 함수의 2번째 인자로 초기값을 추가할 수 있음
 const evens = [2, 4, 6, 8];
-evens.reduce((sum, num) => sum + num) //20
-evens.reduce((sum, num) => sum + num, 100) //120
+evens.reduce((sum, num) => sum + num); // 20
+evens.reduce((sum, num) => sum + num, 100); // 120
