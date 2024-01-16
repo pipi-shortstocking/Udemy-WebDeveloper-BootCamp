3b895dc1aedd84d46d1a1f9adf206e1a648724ef
pipi-shortstocking <jannie435@naver.com>
2024-01-17 01:29:44 +0900

    화살표 함수와 'this'

--- js 강의/Callback_Methods_CODE/ArrowFunctionsThis/app.js
+++ js 강의/Callback_Methods_CODE/ArrowFunctionsThis/app.js
@@ -1,14 +1,45 @@
+// const person = {
+//   firstName: "Viggo",
+//   lastName: "Mortensen",
+//   fullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// const person = {
+//   firstName: "Viggo",
+//   lastName: "Mortensen",
+//   fullName: () => {
+//     // this 키워드가 윈도우 객체를 가리킴
+//     console.log(this);
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// const person = {
+//   firstName: "Viggo",
+//   lastName: "Mortensen",
+//   fullName: () => {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   shoutName: function () {
+//     setTimeout(function () {
+//       console.log(this);
+//       console.log(this.fullName());
+//     }, 3000);
+//   },
+// };
+
 const person = {
-    firstName: 'Viggo',
-    lastName: 'Mortensen',
-    fullName: function () {
-        return `${this.firstName} ${this.lastName}`
-    },
-    shoutName: function () {
-        setTimeout(() => {
-            //keyword 'this' in arrow functions refers to the value of 'this' when the function is created
-            console.log(this);
-            console.log(this.fullName())
-        }, 3000)
-    }
-}
+  firstName: "Viggo",
+  lastName: "Mortensen",
+  fullName: () => {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  shoutName: function () {
+    setTimeout(() => {
+      console.log(this);
+      console.log(this.fullName());
+    }, 3000);
+  },
+};
