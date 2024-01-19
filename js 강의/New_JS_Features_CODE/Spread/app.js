35e9f57e745494439e0520cc73b6c210adaac86f
pipi-shortstocking <jannie435@naver.com>
2024-01-19 14:44:36 +0900

    행렬 리터럴 스프레드 구문

--- js 강의/New_JS_Features_CODE/Spread/app.js
+++ js 강의/New_JS_Features_CODE/Spread/app.js
@@ -1,15 +1,13 @@
 const nums = [13, 4, 5, 21, 3, 3, 1, 2, 7, 6, 4, 2, 53456];
 // SPREAD IN FUNCTIONS
-Math.max(nums) //NaN
-Math.max(...nums) //53456
+Math.max(nums); //NaN
+Math.max(...nums); //53456
 
-
-// SPREAD IN ARRAYS
+// 배열에서의 spread
 const cats = ['Blue', 'Scout', 'Rocket'];
 const dogs = ['Rusty', 'Wyatt'];
 
 const allPets = [...cats, ...dogs];
-
 
 // SPREAD IN OBJECTS
 const feline = { legs: 4, family: 'Felidae' };
@@ -17,10 +15,9 @@ const canine = { isFurry: true, family: 'Caninae' };
 
 const catDog = { ...feline, ...canine };
 
-
 const dataFromForm = {
-    email: 'blueman@gmail.com',
-    password: 'tobias123!',
-    username: 'tfunke'
-}
-const newUser = { ...dataFromForm, id: 2345, isAdmin: false }
+  email: 'blueman@gmail.com',
+  password: 'tobias123!',
+  username: 'tfunke',
+};
+const newUser = { ...dataFromForm, id: 2345, isAdmin: false };
