595c2d9c052ce07318624e20a061fa729419addc
pipi-shortstocking <jannie435@naver.com>
2024-01-19 15:05:59 +0900

    REST 매개변수

--- js 강의/New_JS_Features_CODE/Rest/app.js
+++ js 강의/New_JS_Features_CODE/Rest/app.js
@@ -1,15 +1,15 @@
+// arguments.reduce is not a function -> arguments가 인수객체이기 때문
+// (arguements는 화살표 함수에서도 사용 불가!)
 // function sum() {
-//     return arguments.reduce((total, el) => total + el)
+//   return arguments.reduce((total, el) => total + el);
 // }
 
-// COLLECT THE "REST" IN NUMS:
-function sum(...nums) {
-    return nums.reduce((total, el) => total + el)
-}
-
+// function sum(...nums) {
+//   return nums.reduce((total, el) => total + el);
+// }
 
 function raceResults(gold, silver, ...everyoneElse) {
-    console.log(`GOLD MEDAL GOES TO: ${gold}`)
-    console.log(`SILVER MEDAL GOES TO: ${silver}`)
-    console.log(`AND THANKS TO EVERYONE ELSE: ${everyoneElse}`)
+  console.log(`GOLD MEDAL GOES TO: ${gold}`);
+  console.log(`SILVER MEDAL GOES TO: ${silver}`);
+  console.log(`AND THANKS TO EVERYONE ELSE: ${everyoneElse}`);
 }
