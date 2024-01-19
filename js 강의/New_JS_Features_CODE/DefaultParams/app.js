985f0cebcff075bf22fb7550b9104b130f9f1296
pipi-shortstocking <jannie435@naver.com>
2024-01-19 14:25:04 +0900

    기본 매개변수

--- js 강의/New_JS_Features_CODE/DefaultParams/app.js
+++ js 강의/New_JS_Features_CODE/DefaultParams/app.js
@@ -1,17 +1,12 @@
-// // ==========================================
-// AN OLDER WAY OF ADDING DEFAULT PARAM VALUE
-// ==========================================
-
 // function rollDie(numSides) {
-//     if (numSides === undefined) {
-//         numSides = 6
-//     }
-//     return Math.floor(Math.random() * numSides) + 1
+//   if (numSides == undefined) {
+//     numSides = 6;
+//   }
+
+//   // Math.random()은 0이상 1미만의 난수 반환
+//   return Math.floor(Math.random() * numSides) + 1;
 // }
 
-// ============
-// THE NEW WAY!
-// ============
 function rollDie(numSides = 6) {
   return Math.floor(Math.random() * numSides) + 1;
 }
