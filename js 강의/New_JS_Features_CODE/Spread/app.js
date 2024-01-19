7df3330f0f2735a60348262f9994081f0a1bb7bc
pipi-shortstocking <jannie435@naver.com>
2024-01-19 14:51:57 +0900

    객체 스프레드 구문

--- js 강의/New_JS_Features_CODE/Spread/app.js
+++ js 강의/New_JS_Features_CODE/Spread/app.js
@@ -7,17 +7,18 @@ Math.max(...nums); //53456
 const cats = ['Blue', 'Scout', 'Rocket'];
 const dogs = ['Rusty', 'Wyatt'];
 
-const allPets = [...cats, ...dogs];
+const allPets = [...cats, ...dogs]; // 콘솔에서 직접적으로 요소 추가 가능
 
-// SPREAD IN OBJECTS
+// 객체에서의 spread
 const feline = { legs: 4, family: 'Felidae' };
 const canine = { isFurry: true, family: 'Caninae' };
 
-const catDog = { ...feline, ...canine };
+const catDog = { ...feline, ...canine }; // family에 충돌 발생
 
 const dataFromForm = {
   email: 'blueman@gmail.com',
   password: 'tobias123!',
   username: 'tfunke',
 };
+
 const newUser = { ...dataFromForm, id: 2345, isAdmin: false };
