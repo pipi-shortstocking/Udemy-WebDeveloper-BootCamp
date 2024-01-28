c2eab1ee4fd316ce2e3263dd2e88dd2537b52101
pipi-shortstocking <jannie435@naver.com>
2024-01-29 00:57:19 +0900

    querySelector & querySelectorAll

--- js 강의/DOM_Intro_CODE/Selecting/app.js
+++ js 강의/DOM_Intro_CODE/Selecting/app.js
@@ -5,12 +5,12 @@
 //     "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
 // }
 
-const squareImages = document.getElementsByClassName("square");
+// const squareImages = document.getElementsByClassName("square");
 
-for (let img of squareImages) {
-  img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
-}
+// for (let img of squareImages) {
+//   img.src =
+//     "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+// }
 
 const links = document.querySelectorAll("p a");
 
