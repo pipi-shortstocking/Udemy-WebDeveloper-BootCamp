adf45e63e9d9c232858e7aa3f88f3e78e5541327
pipi-shortstocking <jannie435@naver.com>
2024-02-07 01:39:34 +0900

    속성 조작

--- js 강의/DOM_Intro_CODE/Manipulating/app.js
+++ js 강의/DOM_Intro_CODE/Manipulating/app.js
@@ -3,3 +3,14 @@
 // for (let link of allLinks) {
 //   link.innerText = "I AM A LINK!!!!";
 // }
+
+const firstLink = document.querySelector("a");
+
+// 같은 것을 지목하는 두 명령어의 차이
+
+firstLink.href; // JavaScript를 거침
+// "file://wiki/List_of_chicken_breeds"
+firstLink.getAttribute("href"); // html에서 바로 불러옴
+// "/wiki/List_of_chicken_breeds"
+
+// setAttribute를 통해 속성을 바꿀 수도 있음
