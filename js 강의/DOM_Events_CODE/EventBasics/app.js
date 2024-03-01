c6f055a449a51acb629b72dc5387ac3efe60ad2d
pipi-shortstocking <jannie435@naver.com>
2024-03-01 18:10:30 +0900

    addEventListener

--- js 강의/DOM_Events_CODE/EventBasics/app.js
+++ js 강의/DOM_Events_CODE/EventBasics/app.js
@@ -15,3 +15,25 @@ btn.onmouseenter = scream;
 document.querySelector("h1").onclick = () => {
   alert("you clicked the h1!");
 };
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function () {
+  alert("CLICKED!");
+});
+
+function twist() {
+  console.log("TWIST!");
+}
+function shout() {
+  console.log("SHOUT!");
+}
+
+const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// 위와 같이 onclick만으로는 두 함수 동시 실행 불가
+// tasButton.addEventListener("click", twist, { once: true }); // 한 번만 실행되고 삭제됨
+tasButton.addEventListener("click", twist);
+tasButton.addEventListener("click", shout);
