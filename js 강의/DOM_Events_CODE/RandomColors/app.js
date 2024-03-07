c65f8fbd1df213f8bf93e2e234690d15e0c3e33a
pipi-shortstocking <jannie435@naver.com>
2024-03-07 14:34:29 +0900

    랜덤컬러 연습하기

--- js 강의/DOM_Events_CODE/RandomColors/app.js
+++ js 강의/DOM_Events_CODE/RandomColors/app.js
@@ -1,2 +1,16 @@
 const button = document.querySelector("button");
-button.addEventListener("click", function () {});
+const h1 = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+  const newColor = makeRandColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innerText = newColor;
+});
+
+// 충분한 정도의 길이가 될 경우, 따로 빼놓는 습관을 들이는 것이 좋음!
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
