d7113ed8e365fe783549a25060deea33687e9304
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2024-04-10 01:37:44 +0900

    입력과 변경이벤트

--- js 강의/DOM_Events_CODE/ChangeAndInputEvents/app.js
+++ js 강의/DOM_Events_CODE/ChangeAndInputEvents/app.js
@@ -1,10 +1,12 @@
 const input = document.querySelector('input');
 const h1 = document.querySelector('h1');
 
+// // 뭔가 일어날 때마다(X), 뭔가 변경될 때마다(X)
+// // 그 입력을 떠날 때마다(O)
 // input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
-// })
+//   console.log('ASDFA');
+// });
 
 input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+  h1.innerText = input.value;
+});
