0c246df6c320b6a343e15ff20e7390ed75c75b0f
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2024-04-11 01:10:52 +0900

    이벤트 버블링

--- js 강의/DOM_Events_CODE/Bubbling/app.js
+++ js 강의/DOM_Events_CODE/Bubbling/app.js
@@ -2,16 +2,16 @@ const button = document.querySelector('#changeColor');
 const container = document.querySelector('#container');
 
 button.addEventListener('click', function (e) {
-    container.style.backgroundColor = makeRandColor();
-    e.stopPropagation();
-})
+  container.style.backgroundColor = makeRandColor();
+  e.stopPropagation(); // 이벤트 버블링을 막아줌
+});
 container.addEventListener('click', function () {
-    container.classList.toggle('hide');
-})
+  container.classList.toggle('hide');
+});
 
 const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
-}
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
