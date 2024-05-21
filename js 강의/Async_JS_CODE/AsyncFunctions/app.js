4094301c7e4a8924898a62e5cc63dc6ea2083dc9
pipi-shortstocking <jannie435@naver.com>
2024-05-22 00:20:41 +0900

    비동기 키워드

--- js 강의/Async_JS_CODE/AsyncFunctions/app.js
+++ js 강의/Async_JS_CODE/AsyncFunctions/app.js
@@ -1,107 +1,36 @@
-// async function hello() {
-// }
+// async 자체가 함수를 비동기 함수로 선언하는 키워드
+// async가 붙은 비동기 함수는 자동으로 Promise를 반환함
 
+// async function hello() {}
 
-const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
-}
+// const sing = async () => {
+//   throw "OH NO, PROBLEM!";
+//   return "LA LA LA LA";
+// };
 
-sing()
-    .then(data => {
-        console.log("PROMISE RESOLVED WITH:", data)
-    })
-    .catch(err => {
-        console.log("OH NO, PROMISE REJECTED!")
-        console.log(err)
-    })
-
-
-
+// sing()
+//   .then((data) => {
+//     console.log("PROMISE RESOLVED WITH:", data);
+//   })
+//   .catch((err) => {
+//     console.log("OH NO, PROMISE REJECTED!");
+//     console.log(err);
+//   });
 
 const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
-    throw 'Invalid Password'
-}
+  if (!username || !password) throw "Missing Credentials";
+  if (password === "corgifeetarecute") return "WELCOME!";
+  throw "Invalid Password";
+};
 
-login('todd', 'corgifeetarecute')
-    .then(msg => {
-        console.log("LOGGED IN!")
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log("ERROR!")
-        console.log(err)
-    })
+login("akjsdhsa", "corgifeetarecute")
+  .then((msg) => {
+    console.log("LOGGED IN!");
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log("ERROR!");
+    console.log(err);
+  });
 
-
-
-
-
-
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay)
-    })
-}
-
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
-
-
-async function rainbow() {
-    await delayedColorChange('red', 1000)
-    await delayedColorChange('orange', 1000)
-    await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return "ALL DONE!"
-}
-
-// rainbow().then(() => console.log("END OF RAINBOW!"))
-
-
-async function printRainbow() {
-    await rainbow();
-    console.log("END OF RAINBOW!")
-}
-
-printRainbow();
-
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 2000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
-
-
-async function makeTwoRequests() {
-    try {
-        let data1 = await fakeRequest('/page1');
-        console.log(data1);
-        let data2 = await fakeRequest('/page2');
-        console.log(data2);
-    } catch (e) {
-        console.log("CAUGHT AN ERROR!")
-        console.log("error is:", e)
-    }
-
-}
+// 반환값이 있으면 resolved가 나타나고, 오류가 있으면 오류 값과 함께 rejected가 뜸
