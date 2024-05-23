4ed3d4e2d08deca033f07744ac5ac9736b8b92c7
pipi-shortstocking <jannie435@naver.com>
2024-05-23 17:21:46 +0900

    대기 키워드

--- js 강의/Async_JS_CODE/AsyncFunctions/app.js
+++ js 강의/Async_JS_CODE/AsyncFunctions/app.js
@@ -17,20 +17,78 @@
 //     console.log(err);
 //   });
 
-const login = async (username, password) => {
-  if (!username || !password) throw "Missing Credentials";
-  if (password === "corgifeetarecute") return "WELCOME!";
-  throw "Invalid Password";
-};
+// const login = async (username, password) => {
+//   if (!username || !password) throw "Missing Credentials";
+//   if (password === "corgifeetarecute") return "WELCOME!";
+//   throw "Invalid Password";
+// };
 
-login("akjsdhsa", "corgifeetarecute")
-  .then((msg) => {
-    console.log("LOGGED IN!");
-    console.log(msg);
-  })
-  .catch((err) => {
-    console.log("ERROR!");
-    console.log(err);
-  });
+// login("akjsdhsa", "corgifeetarecute")
+//   .then((msg) => {
+//     console.log("LOGGED IN!");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR!");
+//     console.log(err);
+//   });
 
 // 반환값이 있으면 resolved가 나타나고, 오류가 있으면 오류 값과 함께 rejected가 뜸
+
+// await는 비동기 코드를 쓰면서 동기적으로 보이게 해줌
+// Promise가 값을 반환할 때(해결 될 때)까지 기다리기 위해 비동기 함수의 실행을 일시정지 시킴
+
+// const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
+
+// delayedColorChange("red", 1000)
+//   .then(() => delayedColorChange("orange", 1000))
+//   .then(() => delayedColorChange("yellow", 1000))
+//   .then(() => delayedColorChange("green", 1000))
+//   .then(() => delayedColorChange("blue", 1000))
+//   .then(() => delayedColorChange("indigo", 1000))
+//   .then(() => delayedColorChange("violet", 1000));
+
+// async function rainbow() {
+//   await delayedColorChange("red", 1000);
+//   await delayedColorChange("orange", 1000);
+//   await delayedColorChange("yellow", 1000);
+//   await delayedColorChange("green", 1000);
+//   await delayedColorChange("blue", 1000);
+//   await delayedColorChange("indigo", 1000);
+//   await delayedColorChange("violet", 1000);
+//   return "ALL DONE!";
+// }
+
+// rainbow().then(() => console.log("END OF RAINBOW!"));
+
+// async function printRainbow() {
+//   await rainbow();
+//   console.log("END OF RAINBOW!");
+// }
+
+// printRainbow();
+
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout : (");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
+
+async function makeTwoRequests() {
+  let data1 = await fakeRequest("/page1");
+  console.log(data1);
+}
