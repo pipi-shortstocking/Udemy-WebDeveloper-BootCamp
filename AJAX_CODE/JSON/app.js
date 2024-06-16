1a032e716e85921332a15b1330f83b9147767e15
pipi-shortstocking <jannie435@naver.com>
2024-06-16 16:58:19 +0900

    JSON이란?

--- AJAX_CODE/JSON/app.js
+++ AJAX_CODE/JSON/app.js
@@ -1,7 +1,8 @@
 //THIS IS A STRING OF JSON (NOT AN OBJECT)
-const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`
+const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`;
 
-// THIS IS A JS OBJECT
+// THIS IS A JS OBJECT - 역직렬화
 const parsedData = JSON.parse(data);
 
-
+// 직렬화 : JSON으로 변환하는 것 - JSON.stringify
+// 역직렬화 : JSON을 객체, 해시테이블, 딕셔너리 등으로 변화하는 것 - JSON.parse
