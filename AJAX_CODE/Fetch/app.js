5749b93cb720b10471dbf3f79ccdfc495fb4aefe
pipi-shortstocking <jannie435@naver.com>
2024-07-24 23:30:08 +0900

    Using The Fetch API

--- AJAX_CODE/Fetch/app.js
+++ AJAX_CODE/Fetch/app.js
@@ -1,18 +1,24 @@
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("RESOLVED!", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.log("ERROR!", e);
-  });
+// fetch는 함수로 요청을 만들 수 있고, promise를 지원함
 
-// fetch("https://swapi.dev/api/people/1/")
+const loadStarWarsPeople = async () => {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1/");
+    const data = await res.json();
+    console.log(data);
+
+    const res2 = await fetch("https://swapi.dev/api/people/2/");
+    const data2 = await res2.json();
+    console.log(data2);
+  } catch (e) {
+    console.log("ERROR!!!", e);
+  }
+};
+
+loadStarWarsPeople();
+
+// fetch("https://swapi.dev/api/people/1")
 //   .then((res) => {
-//     console.log("RESOLVED!", res);
+//     console.log("RESOLVED!", res); // fetch가 헤더를 수신하는 즉시 해당 프로미스가 resolve(처리)됨 -> 전체 본문이 표시되지 않는 이유
 //     return res.json();
 //   })
 //   .then((data) => {
@@ -30,17 +36,14 @@ fetch("https://swapi.dev/api/people/1/")
 //     console.log("ERROR!", e);
 //   });
 
-const loadStarWarsPeople = async () => {
-  try {
-    const res = await fetch("https://swapi.dev/api/people/1/");
-    const data = await res.json();
-    console.log(data);
-    const res2 = await fetch("https://swapi.dev/api/people/2/");
-    const data2 = await res2.json();
-    console.log(data2);
-  } catch (e) {
-    console.log("ERROR!!!", e);
-  }
-};
-
-loadStarWarsPeople();
+// fetch("https://swapi.dev/api/people/1")
+//   .then((res) => {
+//     console.log("RESOLVED!", res); // fetch가 헤더를 수신하는 즉시 해당 프로미스가 resolve(처리)됨 -> 전체 본문이 표시되지 않는 이유
+//     res.json().then((data) => {
+//       // json() : res의 객체를 가져오는 함수
+//       console.log("JSON DONE", data);
+//     });
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
